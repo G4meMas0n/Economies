b2e7825c74a4f57c5d8cdcd130a78cdc4ae54316
G4meMason <G4meMas0n>
2021-12-29 15:10:47 +0100

    Initial Commit

---
+++ Economies/src/main/java/com/github/g4memas0n/economies/Economies.java
@@ -0,0 +1,22 @@
+package com.github.g4memas0n.economies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Economies extends JavaPlugin {
+
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
